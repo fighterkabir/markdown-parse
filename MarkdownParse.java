661641dac6ec0c1b00f4c986987a5aa4616b3834
fighterkabir <kshergill336@gmail.com>
2022-02-03 15:55:17 -0800

    i dont even knowww

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,9 +16,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            //if(markdown.substring(openParen + 1, closeParen).contains("https")) {
+            if(markdown.substring(openParen + 1, closeParen).contains("https")) {
                 //toReturn.add(markdown.substring(openParen + 1, closeParen));
-            //}
+            }
             
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
